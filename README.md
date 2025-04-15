3917f626dba684782b00e5bd3e920b8195b56917
Justin Reina <justinmreina@gmail.com>
2025-04-15 09:29:11 -0700

    (C)    relocate projects to proj\        - rename main\ to core\ at root        - add external source directory        - style updates        - compile/debug        - doc updates        - add vs code workspace        - add c_cpp_properties.json

--- README.md
+++ README.md
@@ -1,16 +1,73 @@
 # esp32_proj
 
-ESP32 Template for new project creation
+ESP32 Template for new project creation.
+
+Place project into _proj\\_ with _main\\_ renamed _core\\_ using external source directories (_lbr\\_)
 
 # Procedure
 
-1. Create a new Espressif-IDE project, with VS Code project alongside no changes
-
-* _'New'_ -> _'Espressif IDF Project'_ using the _hello_world_ template
-
-* Name as _'esp32_reloc_dirs'_ using main.c with compile & debug (#818B)
+1. Create a new project _'New'_ -> _'Espressif IDF Project'_ using the _hello_world_ template
 
 * Preparing for USB Console printf() & FreeRTOS
+
+* Rename as _'esp32_reloc_dirs'_ using main.c with compile & debug (#818B)
+
+2. Rename _main\\_ source directory
+
+# 1 - Rename _main\\_ Directory
+
+The build system provides special treatment to the main component. It is a component that gets automatically added to the build provided that it is in the expected location, PROJECT_DIR/main. All other components in the build are also added as its dependencies, saving the user from hunting down dependencies and providing a build that works right out of the box. Renaming the main component causes the loss of these behind-the-scenes heavy lifting, requiring the user to specify the location of the newly renamed component and manually specify its dependencies. Specifically, the steps to renaming main are as follows:
+
+1. Rename main directory (e.g. _'src\\'_)
+
+2. Set EXTRA_COMPONENT_DIRS in the project CMakeLists.txt to include the renamed main directory
+
+	a. Locate the Project CMakeLists.txt at the root
+	
+	b. create(set) or append(list) to EXTRA_COMPONENT_DIRS
+```	
+	 set(EXTRA_COMPONENT_DIRS ${CMAKE_CURRENT_LIST_DIR}/src)
+```
+
+3. Specify the dependencies in the renamed component's CMakeLists.txt file via REQUIRES or PRIV_REQUIRES arguments on component registration (iff asked)
+
+# 2 - Renaming _build\\_ Directory
+
+Espressif uses build\ as a default and is not described well how to change this on a project-specific basis
+
+## Normal Procedure
+
+```
+	cmake -B your_target_build_dir_name
+```
+
+Or
+
+```
+	set(CMAKE_BINARY_DIR ${CMAKE_BINARY_DIR}/../your_target_build_dir_name)
+```	
+
+But even here ESP-IDF still leaves a build dir, no matter what
+
+## Mitigation
+
+Compile project as normal with build\ at root, including build\ in your commit history
+
+## Result
+
+1. Hide build\ from the IDE (Project Explorer -> Filters and Customization -> User Filters -> New) & File Explorer
+
+2. Clean then delete build\ for recompiles
+
+3. Leave build\ alone! This is how ESP-IDF works
+
+# 3 - Add _lbr\\_ Directory
+
+1. Create new directory source & interface files for use
+
+2. Add a CMakeLists.txt to this dir using the src\CMakeLists.txt for reference
+
+3. Add reference to this dir in root CMakeLists.txt 'set(EXTRA_COMPONENT_DIRS ...)'
 
 ## How to use example
 
@@ -20,7 +77,6 @@ Select the instructions depending on Espressif chip installed on your developmen
 
 - [ESP32 Getting Started Guide](https://docs.espressif.com/projects/esp-idf/en/stable/get-started/index.html)
 - [ESP32-S2 Getting Started Guide](https://docs.espressif.com/projects/esp-idf/en/latest/esp32s2/get-started/index.html)
-
 
 ## Example folder contents
 
---
+++ core/main.c
@@ -0,0 +1,136 @@
+/**************************************************************************************************/
+/** @file     main.c
+ *  @brief    Project setup & directory relocation example 
+ *  @details  Serving reference for other projects
+ *
+ *  @author   Justin Reina, Firmware Engineer
+ *  @created  3/31/25
+ *  @last rev 4/16/25
+ *
+ *  @note   Private functions & variables are declared static
+ *
+ *  @section    Opens
+ *      - Espressif-IDE compile/debug (reinstall & stopped working?)
+ *      - VS Code debug code stepping (JTAG)
+ *
+ *  @section    Legal Disclaimer
+ *      ©2025 Justin Reina. All rights reserved. All contents of this source file and/or any other
+ *      related source files are the explicit property of Justin Reina. Do not distribute.
+ *      Do not copy.
+ */
+/**************************************************************************************************/
+
+//************************************************************************************************//
+//                                            INCLUDES                                            //
+//************************************************************************************************//
+
+//Standard Library Includes
+#include <stdio.h>
+#include <inttypes.h>
+
+//Library Includes
+#include "esp_chip_info.h"
+#include "esp_flash.h"
+#include "esp_system.h"
+
+//SDK Includes
+#include "sdkconfig.h"
+
+//FreeRTOS Includes
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+//Project Includes
+#include "utils.h"
+#include "main.h"
+
+
+//************************************************************************************************//
+//                                          PUBLIC FUNCTIONS                                      //
+//************************************************************************************************//
+
+/**************************************************************************************************/
+/** @fcn        void app_main(void)
+ *  @brief      FreeRTOS task for main application
+ *  @details    Called by FreeRTOS scheduler when started
+ *
+ *  @section    Purpose
+ *      Unlike normal FreeRTOS tasks, or embedded C main functions, the app_main() task is allowed 
+ *      to return. If this happens, The task is cleaned up and the system will continue running 
+ *      with other RTOS tasks scheduled normally. Therefore, it is possible to implement app_main 
+ *      as either a function that creates other application tasks and then returns, or as a main 
+ *      application task itself. app_main() has a fixed RTOS priority, one higher than the minimum
+ *
+ *  @pre    second stage bootloader
+ *  @post   esp_restart()
+ */
+/**************************************************************************************************/
+void app_main(void) {
+    
+    //Locals
+    unsigned major_rev;                             /* Chip major revision                        */
+    unsigned minor_rev;                             /* Chip minor revision                        */
+    uint32_t flash_size;                            /* Flash memory size (Bytes)                  */
+    esp_err_t stat;                                 /* SDK response status code                   */
+    esp_chip_info_t chip_info;                      /* Info about the chip                        */
+
+
+    //---------------------------------------- Initialize ----------------------------------------//
+    printf("Hello esp!\n");
+
+    //Get Info
+    esp_chip_info(&chip_info);
+    
+    //Retrieve
+    major_rev = (chip_info.revision / 100);         /* divisor                                    */
+    minor_rev = (chip_info.revision % 100);         /* remainder                                  */
+
+    /* Print chip information */
+    printf("This is %s chip with %d CPU core(s), %s%s%s%s, ",
+              CONFIG_IDF_TARGET,
+              chip_info.cores,
+              (chip_info.features & CHIP_FEATURE_WIFI_BGN)   ? "WiFi/" : "",
+              (chip_info.features & CHIP_FEATURE_BT)         ? "BT"    : "",
+              (chip_info.features & CHIP_FEATURE_BLE)        ? "BLE"   : "",
+              (chip_info.features & CHIP_FEATURE_IEEE802154) ? ", 802.15.4 (Zigbee/Thread)" : ""
+          );
+                   
+    printf("silicon revision v%d.%d, ", major_rev, minor_rev);
+        
+    //Get Size
+    stat = esp_flash_get_size(NULL, &flash_size);
+    
+    //Safety
+    if(stat != ESP_OK) {    
+        printf("Get flash size failed");    
+        return;
+    }
+
+    printf("%" PRIu32 "MB %s flash\n", 
+              flash_size / (uint32_t)(1024 * 1024),
+              (chip_info.features & CHIP_FEATURE_EMB_FLASH)  ? "embedded" : "external"
+          );
+
+    printf("Minimum free heap size: %" PRIu32 " bytes\n", esp_get_minimum_free_heap_size());
+
+
+    //------------------------------------------ Operate -----------------------------------------//
+
+    for(int i = 10; i >= 0; i--) {
+        
+        printf("Restarting in %d seconds...\n", i);
+        
+        utils_test_fcn();
+        
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+    
+    
+    //------------------------------------------ Restart -----------------------------------------//
+    printf("Restarting now.\n");
+
+    fflush(stdout);
+
+    esp_restart();
+}
+
